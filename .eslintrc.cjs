8c314ab36e7a15400388fec2afd8a2cce4fa79d6
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-07-27 13:39:16 -0300

    chore: Improve Prettier comment in .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,8 @@ module.exports = {
   },
   /**
    * In order to display Prettier errors through ESLint, 'plugin:prettier/recommended' is added in the property below.
-   * Switch it for 'prettier' if you do not wish this behavior (the 'eslint-plugin-prettier' dependency can be removed too).
+   * Switch it for 'prettier' and remove 'eslint-plugin-prettier' from 'plugins' if you do not wish this behavior
+   * (the 'eslint-plugin-prettier' dependency can be removed too).
    */
   extends: ['plugin:svelte/prettier', 'plugin:prettier/recommended'],
   plugins: ['eslint-plugin-prettier'],
