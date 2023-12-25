79576168e50666e727b652dcec9a254664b56e05
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-12-25 20:14:46 -0300

    refactor: Move main page markup to +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import svelteLogo from '$assets/images/svelte-logo.svg';
+	import './styles.css';
+</script>
+
+<main class="app-main">
+	<div class="container">
+		<slot />
+	</div>
+</main>
+<footer class="app-footer">
+	<div class="container">
+		<a
+			href="https://github.com/rodrigodagostino/vite-svelte-ts-template"
+			target="_blank"
+			rel="noreferrer"
+		>
+			Made with <img src={svelteLogo} alt="SvelteKit" /> by Rodrigo D’Agostino
+		</a>
+	</div>
+</footer>
+
+<style lang="scss">
+	.container {
+		max-width: 54rem;
+		margin: 0 auto;
+		padding: 1rem;
+		position: relative;
+	}
+
+	.app-main {
+		margin-top: 24vh;
+	}
+
+	.app-footer {
+		font-size: 0.875rem;
+		text-align: center;
+		margin-top: auto;
+
+		.container {
+			display: flex;
+			justify-content: center;
+			padding: 1rem 0;
+		}
+
+		a {
+			display: flex;
+			color: var(--gray-400);
+
+			&:focus,
+			&:hover {
+				color: var(--gray-600);
+			}
+		}
+
+		img {
+			height: 1.25rem;
+			width: auto;
+			vertical-align: middle;
+			margin: 0 0.25rem;
+			display: inline-block;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import './styles.css';
 	import svelteLogo from '$assets/images/svelte-logo.svg';
 </script>
 
@@ -7,39 +6,19 @@
 	<title>Welcome to SvelteKit</title>
 </svelte:head>
 
-<main class="app-main">
-	<div class="container">
-		<div>
-			<a href="https://kit.svelte.dev" target="_blank" rel="noreferrer">
-				<img src={svelteLogo} alt="SvelteKit" />
-			</a>
-		</div>
-		<h1>Welcome to SvelteKit</h1>
-		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation.</p>
-	</div>
-</main>
-<footer class="app-footer">
-	<div class="container">
-		<a
-			href="https://github.com/rodrigodagostino/vite-svelte-ts-template"
-			target="_blank"
-			rel="noreferrer"
-		>
-			Made with <img src={svelteLogo} alt="SvelteKit" /> by Rodrigo D’Agostino
-		</a>
-	</div>
-</footer>
+<div class="page">
+	<a href="https://kit.svelte.dev" target="_blank" rel="noreferrer">
+		<img src={svelteLogo} alt="SvelteKit" />
+	</a>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation.</p>
+</div>
 
 <style lang="scss">
-	.container {
-		max-width: 54rem;
-		margin: 0 auto;
-		padding: 1rem;
-		position: relative;
-	}
-
-	.app-main {
-		margin-top: 24vh;
+	.page {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 
 		img {
 			width: 6rem;
@@ -49,36 +28,6 @@
 
 		h1 {
 			margin: 2rem 0 1rem;
-		}
-	}
-
-	.app-footer {
-		font-size: 0.875rem;
-		text-align: center;
-		margin-top: auto;
-
-		.container {
-			display: flex;
-			justify-content: center;
-			padding: 1rem 0;
-		}
-
-		a {
-			display: flex;
-			color: var(--gray-400);
-
-			&:focus,
-			&:hover {
-				color: var(--gray-600);
-			}
-		}
-
-		img {
-			height: 1.25rem;
-			width: auto;
-			vertical-align: middle;
-			margin: 0 0.25rem;
-			display: inline-block;
 		}
 	}
 </style>
